8d4efe06dfbeb0b6d83f1f3e882d913738cbcdbc
Miran Hrupački <mhrupacki@etfos.hr>
2020-05-22 20:05:58 +0200

    added view for map

--- weather-app/MapViewController/MapViewController.swift
+++ weather-app/MapViewController/MapViewController.swift
@@ -25,12 +25,11 @@ class MapViewController: UIViewController {
         return pointer
     }()
     
-    let currentLocation: UILabel = {
+    let addressLabel: UILabel = {
         let location = UILabel()
         location.translatesAutoresizingMaskIntoConstraints = false
         location.backgroundColor = .white
         location.textColor = .black
-        location.text = "avc"
         location.numberOfLines = 0
         location.adjustsFontSizeToFitWidth = true
         location.textAlignment = .center
@@ -40,14 +39,14 @@ class MapViewController: UIViewController {
     
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 7000
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(mapView)
         mapView.addSubview(pointer)
-        mapView.addSubview(currentLocation)
-        
+        mapView.addSubview(addressLabel)
         setupUI()
         checkLocationService()
     }
@@ -66,10 +65,10 @@ class MapViewController: UIViewController {
             maker.centerX.equalTo(mapView.snp.centerX)
         }
         
-        currentLocation.snp.makeConstraints{(maker) in
+        addressLabel.snp.makeConstraints{(maker) in
             maker.leading.trailing.equalToSuperview()
             maker.bottom.equalToSuperview()
-            maker.height.equalTo(50)
+            maker.height.equalTo(100)
         }
     }
     
@@ -97,9 +96,7 @@ class MapViewController: UIViewController {
     func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
+            startTrackingUserLocation()
         case .denied:
             break
         case .notDetermined:
@@ -110,11 +107,57 @@ class MapViewController: UIViewController {
             break
         }
     }
+    
+    func startTrackingUserLocation() {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
 }
 
 extension MapViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else { return }
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return}
+            
+            if let _ = error {
+                //alert
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                //alert
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetName) \(streetNumber)"
+            }
+        }
     }
 }
